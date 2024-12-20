5d0e414e3dafe8d8559f6388ab762ed398ecaa82
tony-s-h <tony.s.hancock@gmail.com>
2024-12-20 08:42:48 -0500

    update jsx

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,28 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0
+      };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+    <>
+    <div className='content-rating'>
+        <p>
+        ---Add text here---
+        </p>
+        <div className='rating-buttons'>
+        <button className="like-button">
+            Like ({this.state.likes})
+        </button>
+        <button className="dislike-button">
+            Dislike ({this.state.dislikes})
+        </button>
+        </div>
+    </div>
+    </>
     );
   }
 }
